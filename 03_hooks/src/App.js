dd23965b8f0399221fa5753df9e6b89111dd35e3
JungPig <pig.ohgiraffers@gmail.com>
2024-09-25 09:30:58 +0900

    react

---
+++ 03_hooks/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import UseEffectBasic from "./01_useEffect/01_useEffect-basic";
+import UseEffectMount from "./01_useEffect/02_useEffect-mount";
+import UseEffectUpdate from "./01_useEffect/03_useEffect-update";
+import { Container } from "./01_useEffect/04_useEffect-cleanup";
+import { TimeApp } from "./01_useEffect/05_timer";
+
+function App() {
+  const [message,setMessage] = useState("안녕하세요");
+
+  return (
+    <>
+      {/* <UseEffectBasic message={message}/> */}
+      {/* <UseEffectMount/> */}
+      {/* <UseEffectUpdate/> */}
+      <TimeApp/>
+    </>
+  );
+}
+
+export default App;
