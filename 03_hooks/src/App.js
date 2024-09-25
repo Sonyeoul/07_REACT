2d030b45558b3b3d7a9d8593b820c6a27717dfe4
JungPig <pig.ohgiraffers@gmail.com>
2024-09-25 12:44:18 +0900

    react

--- 03_hooks/src/App.js
+++ 03_hooks/src/App.js
@@ -4,6 +4,9 @@ import UseEffectMount from "./01_useEffect/02_useEffect-mount";
 import UseEffectUpdate from "./01_useEffect/03_useEffect-update";
 import { Container } from "./01_useEffect/04_useEffect-cleanup";
 import { TimeApp } from "./01_useEffect/05_timer";
+import UseReducerBasic from "./02_useReducer/01_useReducer-basic";
+import ReducerFormControl from "./02_useReducer/02_reducer-form-controll";
+import ReducerQuestion from "./02_useReducer/03_question";
 
 function App() {
   const [message,setMessage] = useState("안녕하세요");
@@ -13,7 +16,10 @@ function App() {
       {/* <UseEffectBasic message={message}/> */}
       {/* <UseEffectMount/> */}
       {/* <UseEffectUpdate/> */}
-      <TimeApp/>
+      {/* <TimeApp/> */}
+      {/* <UseReducerBasic/> */}
+      {/* <ReducerFormControl/> */}
+      <ReducerQuestion/>
     </>
   );
 }
