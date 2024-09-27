6f575384880afb77d48bb9566394499e392857aa
JungPig <pig.ohgiraffers@gmail.com>
2024-09-27 17:07:34 +0900

    react

---
+++ 04_async/src/App.js
@@ -0,0 +1,19 @@
+import ApiComponent from "./02_api/02_fetch-then";
+import AxiosCallComponent from "./02_api/03_axios";
+import EmojiContainer from "./02_api/04_github-emoji";
+import Container from "./03_data-component/01_rendering-component";
+import Weather from "./03_data-component/02_weather-promise";
+
+function App() {
+  return (
+    <>
+    {/* <ApiComponent/> */}
+    {/* <AxiosCallComponent/> */}
+    {/* <EmojiContainer/> */}
+    {/* <Container/> */}
+    <Weather/>
+    </>
+  );
+}
+
+export default App;
